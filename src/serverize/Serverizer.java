d1885c5321c9aa1110e0e12fbf92528226621dcf
Xiao Jia <stfairy@gmail.com>
2013-07-16 21:51:21 +0800

    print usage

--- src/serverize/Serverizer.java
+++ src/serverize/Serverizer.java
@@ -8,15 +8,24 @@ public class Serverizer {
 
     public static void main(String[] args) throws Exception {
 
+        if (args.length != 2) {
+            System.err.println("Usage:   [port] [program]");
+            System.err.println("Example:  9999  /usr/local/bin/irb");
+            System.exit(1);
+        }
+
+        int port = Integer.valueOf(args[0]);
+        String program = args[1];
+
         ServletContextHandler context = new ServletContextHandler();
         context.setContextPath("/");
-        context.addServlet(new ServletHolder(new HomeServlet(args[0])), "/");
+        context.addServlet(new ServletHolder(new HomeServlet(program)), "/");
         context.addServlet(new ServletHolder(new KeyboardInputServlet()), "/input");
         context.addServlet(new ServletHolder(new ScreenOutputServlet()), "/output");
         context.addServlet(new ServletHolder(new SessionUnloadServlet()), "/unload");
         context.addServlet(new ServletHolder(new ScriptServlet()), "/script.js");
 
-        Server server = new Server(9999);
+        Server server = new Server(port);
         server.setHandler(context);
         server.start();
         server.join();
